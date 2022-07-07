fef7e9a08008d7f6e013db6ca86efc8e43965272
Dmitriy <dimashkavysotskyy@gmail.com>
2022-07-08 00:23:46 +0300

    docker 2.1

---
+++ src/GameNews.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GameNews.ApplicationCore;
+using GameNews.Infrastructure.Entities;
+using GameNews.Infrastructure.Context;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GameNews.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<BlogEntity> Get()
+        {
+            return _dbContext.blogs.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post(BlogEntity blogEntity)
+        {
+            _dbContext.blogs.Add(blogEntity);
+            _dbContext.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
